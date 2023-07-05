e5857814da6372e2bb67d07a34fd0379276677a2
Jason Cho <jason.cho2@ibm.com>
2023-07-05 12:59:56 -0700

    build.sh edits for operator to show on operatorhub for ppc64le
    Signed-off-by: Jason Cho <jason.cho2@ibm.com>

--- hack/build.sh
+++ hack/build.sh
@@ -16,8 +16,61 @@ export OPERATOR_NAME='observability-operator'
 export REGISTRY=${REGISTRY:-'quay.io'}
 export NAMESPACE=${NAMESPACE:-'rhobs'}
 export TAG=${TAG:-'0.0.23'}
+export CSV_PATH=${CSV_PATH:-'bundle/manifests/observability-operator.clusterserviceversion.yaml'}
+export ANNOTATIONS_PATH=${ANNOTATIONS_PATH:-'bundle/metadata/annotations.yaml'}
+
+function cleanup {
+	# shellcheck disable=SC2046
+	if [ -x $(command -v git >/dev/null 2>&1) ]; then
+		git checkout "${CSV_PATH}" >/dev/null 2>&1
+		git checkout "${ANNOTATIONS_PATH}" >/dev/null 2>&1
+	fi
+}
+
+trap cleanup EXIT
+
+# prints pre-formatted info output.
+function info {
+	echo "INFO $(date '+%Y-%m-%dT%H:%M:%S') $*"
+}
+
+# prints pre-formatted error output.
+function error {
+	>&2 echo "ERROR $(date '+%Y-%m-%dT%H:%M:%S') $*"
+}
+
+function digest() {
+	declare -n ret=$2
+	IMAGE=$1
+	docker pull "${IMAGE}"
+	# shellcheck disable=SC2034
+	ret=$(docker inspect --format='{{index .RepoDigests 0}}' "${IMAGE}")
+}
 
 docker buildx build --push --platform "linux/amd64,linux/ppc64le" -f build/Dockerfile -t "${REGISTRY}/${NAMESPACE}/observability-operator:${TAG}" .
+digest "${REGISTRY}/${NAMESPACE}/observability-operator:${TAG}" OPERATOR_DIGEST
+
+# need exporting so that yq can see them
+export OPERATOR_DIGEST
+
+# prepare operator files, then build and push operator bundle and catalog
+# index images.
+
+yq eval -i '
+	.metadata.name = strenv(OPERATOR_NAME) |
+	.metadata.annotations.version = strenv(TAG) |
+	.metadata.annotations.containerImage = strenv(OPERATOR_DIGEST) |
+	.metadata.labels += {"operatorframework.io/arch.amd64": "supported", "operatorframework.io/arch.ppc64le": "supported", "operatorframework.io/os.linux": "supported"} |
+	del(.spec.replaces) |
+	.spec.install.spec.deployments[0].name = strenv(OPERATOR_NAME) |
+	.spec.install.spec.deployments[0].spec.template.spec.containers[0].image = strenv(OPERATOR_DIGEST)
+	' "${CSV_PATH}"
+
+yq eval -i '
+	.annotations."operators.operatorframework.io.bundle.channel.default.v1" = "test" |
+	.annotations."operators.operatorframework.io.bundle.channels.v1" = "test"
+	' "${ANNOTATIONS_PATH}"
+
 make bundle-image
 
 AMD64_DIGEST=$(skopeo inspect --raw  docker://${REGISTRY}/${NAMESPACE}/observability-operator-bundle:${TAG} | \
